a896993c4422429e30e3be7e0d7ad57e629a3c37
AISH\HP <omkarnaikare10@gmail.com>
2025-05-19 17:10:18 +0530

    defaults

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:speridian/view/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +33,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Splash(),
+      builder: EasyLoading.init(),
     );
   }
 }
